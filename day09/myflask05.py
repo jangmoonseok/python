c62f91814e41a411fa17ebe68132a4d6b2be206c
DESKTOP-BP73NMV\PC-25 <cacao621@naver.com>
2022-05-11 11:57:25 +0900

    day09 flask crud

--- day09/myflask05.py
+++ day09/myflask05.py
@@ -13,5 +13,44 @@ def ajax():
     emps = dao.myselects()
     return jsonify({'emps': emps})
 
+@app.route('/ajaxDetail')
+def ajaxDetail():
+    e_id = request.args.get("e_id")
+    
+    emp = dao.myselect(e_id)
+    return jsonify({'emp' : emp})
+
+@app.route('/ajaxAdd', methods=["POST"])
+def ajaxAdd():
+    data = request.get_json()
+    e_id = data['e_id']
+    e_name = data['e_name']
+    sex = data['sex']
+    addr = data['addr']
+    cnt = dao.myinsert(e_id, e_name, sex, addr)
+    dao.conn.commit()
+    
+    return jsonify({'cnt' : cnt})
+
+@app.route('/ajaxDelete', methods=["GET"])
+def ajaxDelete():
+    e_id = request.args.get('e_id')
+    cnt = dao.mydelete(e_id)
+    dao.conn.commit()
+    
+    return jsonify({'cnt' : cnt})
+
+@app.route('/ajaxUpdate', methods=["POST"])
+def ajaxUpdate():
+    data = request.get_json()
+    e_id = data['e_id']
+    e_name = data['e_name']
+    sex = data['sex']
+    addr = data['addr']
+    values = [e_name, sex, addr]
+    cnt = dao.myupdate(e_id, values)
+    dao.conn.commit()
+    
+    return jsonify({'cnt' : cnt})
 if __name__ == '__main__':
     app.run(debug=True)
